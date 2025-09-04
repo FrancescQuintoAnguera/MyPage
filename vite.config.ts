f62edb96d6ff1e5e5f71cd660dfdf301d4f509a4
FrancescQuintoAnguera <francescquinto@iesmontsia.org>
2025-09-04 22:41:22 +0200

    Adding path mapping and arcquitecture for developing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://www.francescquinto.dev'
+  base: 'https://www.francescquinto.dev',
+  resolve:{
+    alias:{
+      "@": path.resolve(__dirname, './src')
+    }
+  }
+
 })
